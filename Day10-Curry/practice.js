9485c4b0c6300ee0805657fb08b3a241482231b7
Megha Gupta <megha.gupta2205@gmail.com>
2025-06-19 15:16:18 -0700

    update day10 - curry

--- Day10-Curry/practice.js
+++ Day10-Curry/practice.js
@@ -1,56 +1,42 @@
-/**
- * @param {Function} fn
+
+/** * @param {Function} fn
  * @return {Function}
- */
-var once = function(fn) {
-    let called = false;
-    return function(...args){
-        called = true;
-        if (called) {
-            console.log("Function has already been called");
-            return undefined;
-        } else {
-            console.log("Calling function for the first time");
-            called = true;
-            return fn(...args);
-        }
+ * 
+ * 
+ * **/
 
-        // Alternative implementation
-        // if (called) {
-        //     return undefined;
-        // } else {
-        //     called = true;
-        //     return fn.apply(this, args);  
-        // }    
+/**higher-order function that takes a function `fn` and returns a new function that can be called with any number of arguments, returning the result of calling `fn` with those arguments.
+ * The returned function can be called multiple times, accumulating the arguments until it is finally called with no arguments, at which point it will return the result of calling `fn` with all accumulated arguments.
+**/
+
+
+var curry = function(fn) {
+  let nums = []// TRY TO ACCUMULATE ALL THE PARAMS 1,2,3
+ return function curries(...args) {
+  nums = [...nums, ...args]; // accumulate the args
+    // we have to check if function and list of args reqyuired to run the function is equal then we can call the function 
+    if (fn.length === nums.length) {
+      const result = fn(...nums);
+      // reset nums to empty array for next call
+      console.log("Function called with args:", nums, "Result:", result);
+      nums = [];
+      // return the result of the function call
+      return fn(...nums);
+    } else {
+      //when we dont have enouygh args to call the function
+      // we can return a new function that will accept more args
+      console.log("other nums: ",...nums)
+      return curries
+      
     }
-};
+  
+
+ }
 
 
-  let fn = (a,b,c) => (a + b + c)
- let onceFn = once(fn)
- 
-  console.log(onceFn(1,2,3)); // 6
-  console.log(onceFn(2,3,6)); // returns undefined without calling fn
- 
+}
 
-
-  // 
-  const persone = {
-    name: "John",
-    age: 30,
-    greet() {
-        console.log(`Hello, my name is ${this.name} and I am ${this.age} years old.`);
-    }
-  };
-
-  function callGreet(greetting) {
-    console.log(`${greetting} ${this.name}`);
-  } 
-
-  // Using call, apply, and bind
-  // different ways to call the function with the context of `persone`
-
-  callGreet.call(persone, "Hi"); // Hi John
-  callGreet.apply(persone, ["Hello"]); // Hello John
-  const boundGreet = callGreet.bind(persone, "Hey");
-  boundGreet(); // Hey John
+function sum(a,b,c){return a + b + c;}
+const curries = curry(sum);
+console.log(curries(1)(2)(3)); // 6
+console.log(curries(4, 5)(8)); // 6
--- README.md
+++ README.md
@@ -17,8 +17,8 @@ This repository tracks my progress through the [NeetCode JavaScript Challenge](h
 | 6   | CustomReduce           | [Day06](Day06-CustomReduce/practice.js)          | [Notes](Day06-CustomReduce/notes.md)           |
 | 7   | FunctionComposition    | [Day07](Day07-FunctionComposition/practice.js)   | [Notes](Day07-FunctionComposition/notes.md)    |
 | 8   | OneFunctionCall        | [Day08](Day08-OneFunctionCall/practice.js)       | [Notes](Day08-OneFunctionCall/notes.md)        |
-| 9   | memoize                | [Day09](Day09-Memoize/practice.js)             | [Notes](Day09-Memoize/notes.md)              |
-| 10  | curry       | [Day11](Day11-CancelInterval/practice.js)        | [Notes](Day11-CancelInterval/notes.md)         |
+| 9   | Memoize                | [Day09](Day09-Memoize/practice.js)             | [Notes](Day09-Memoize/notes.md)              |
+| 10  | Curry       | [Day10](Day10-Curry/practice.js)        | [Notes](Day10-Curry/notes.md)         |
 | 11  | Sleep      | [Day12](Day12-Debounce/practice.js)              | [Notes](Day12-Debounce/notes.md)               |
 | 12  | Promise time limit     | [Day13](Day13-Throttle/practice.js)              | [Notes](Day13-Throttle/notes.md)               |
 
